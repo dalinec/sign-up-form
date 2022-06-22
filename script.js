2b71a9e841a729fe82ac03471f9d69cf0722a523
Matej Kukolik <dalinec123@gmail.com>
2022-06-22 12:17:29 +0200

    Finishing touch

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const valid1 = document.getElementById("validation");
 function validatePass(){
     if (password.value != confirmPassword.value || (password.value === '' && confirmPassword.value === '')) {
         confirmPassword.setCustomValidity ("Error : Passwords don't match. Please try again.");
-        password.style.borderColor = "#C13838";
-        confirmPassword.style.borderColor = "#C13838";
+        password.style.border = "2px solid #C13838";
+        confirmPassword.style.border = "2px solid #C13838";
         valid1.textContent = "*Passwords do not match";
         valid1.classList.add('error');
     } else {
@@ -15,8 +15,8 @@ function validatePass(){
         valid1.textContent = '';
     }
     if (password.value === confirmPassword.value) {
-        password.style.borderColor = '#3cd33c';
-        confirmPassword.style.borderColor = '#3cd33c';
+        password.style.border = '2px solid #3cd33c';
+        confirmPassword.style.border = '2px solid #3cd33c';
     }
 }
 
@@ -26,8 +26,8 @@ confirmPassword.onkeyup = validatePass;
 window.onload = redPass;
 
 function redPass() {
-    password.style.borderColor = "#C13839";
-    confirmPassword.style.borderColor = "#C13838";
+    password.style.border = "2px solid #C13839";
+    confirmPassword.style.border = "2px solid #C13838";
     valid1.textContent = "*Passwords do not match";
     valid1.classList.add('error');
 }
